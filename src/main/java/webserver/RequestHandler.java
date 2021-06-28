96f50b8dfc7a675c254db8bda4605f7579c5e399
chulhan <lch93609@naver.com>
2021-06-28 14:01:18 +0900

    2021-06-28 해당 페이지 url 이동 가능하게 만들어 줌

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -29,26 +29,41 @@ public class RequestHandler extends Thread {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-//            byte[] body = "Hello World".getBytes();
-//            response200Header(dos, body.length);
-//            responseBody(dos, body);
-            
+
             // HTTP 요청을 읽을 수 있게 InputStream 을 BufferedReader 로 바꾸기 2021-06-28
-            
+            // 주소가 없을시 접속이 안됨 해결  할 것 !!
             BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
             
             String line = br.readLine();
             String [] tokens = line.split(" ");
             
             for(int i=0; i<tokens.length; i++) {
-            	System.out.println("token : " + tokens[i]);
+            	System.out.println("token : " + tokens[i] +" 번호 확인 : " + i);
             }
             
             String url = tokens[1];
-            byte [] body = Files.readAllBytes(new File("./webapp" + url).toPath() );
-            response200Header(dos, body.length);
-            responseBody(dos, body);			
-            		
+            
+            byte[] body;
+            // 뒤 주소가 없을 시 Default로 Hello World 출력하게 만듦
+            if( url.equals("/")) {
+            	body = "Hello World".getBytes();
+            	response200Header(dos, body.length);
+            	responseBody(dos, body);
+            
+            // 뒤 주소가 있을 시 해당 url로 이동하게 만들어 줌 
+            } else {
+            	body = Files.readAllBytes(new File("./webapp" + url).toPath() );
+            	 response200Header(dos, body.length);
+                 responseBody(dos, body);			
+            }
+            
+            
+            
+            
+            
+            
+            
+            
             // line while문 돌려 HTTP 마지막 Header 확인 2021-06-28
 //            while( !"".equals(line) ) {
 //            	
@@ -60,7 +75,7 @@ public class RequestHandler extends Thread {
 //            	}
 //            	
 //            }
- 
+            
             
         } catch (IOException e) {
             log.error(e.getMessage());
