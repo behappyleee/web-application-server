dc8cf3361255617c0c3006b9b375ffc189899ba0
lee <dhfhfkwhdk@naver.com>
2021-12-28 23:38:38 +0900

    Post 회원방식 회원가입 (구현 중)

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -46,6 +46,7 @@ public class RequestHandler extends Thread {
             System.out.println("line " + line);
             
             String [] tokens = line.split(" ");
+            // 첫번째 Token 에서 url 을 얻는다 
             String url = tokens[1];
             
             // TODO While 문 주는 이유 고민하여 보기 !
@@ -70,45 +71,47 @@ public class RequestHandler extends Thread {
             // 뒤 주소가 있을 시 해당 url로 이동하게 만들어 줌 
             } else {
             	 // 쿼리스트링이 있을 시 
-//            	 if(url.contains("?")) {	
-//            		 // 쿼리 스트링이 있을 시 잘라버림 	
-//            		 int index = url.indexOf("?");
-//            		 
-//            		 String requestPath = url.substring(0, index);
-//            		 String params = url.substring(index+1);
-//            		 
-//            		 System.out.println("User 클래스에 담기 " + requestPath + "parmas : " + params);
-//            		 
-//            		 Map <String, String> paramCheck = HttpRequestUtils.parseQueryString(params);
-//            		 
-//            		 System.out.println("값 parsing Check 하기" + paramCheck);
-//            		 
-//            		 for(String keys : paramCheck.keySet()) {
-//            			 System.out.println("값 Key Check : " + keys + " 값 Value Check " + paramCheck.get(keys) );
-//            		 }
-//            		 
-//            		 User user = new User(paramCheck.get("userId") , paramCheck.get("password") , paramCheck.get("name"), paramCheck.get("email"));
-//            		 System.out.println("User Class 에 잘 담겼는지 Check : " + user);
-//            		 	
-//            		 DataBase.addUser(user);
-//            		 
-//            		 System.out.println("DB 들어간 값 확인 " + DataBase.findUserById(paramCheck.get("userId")) );
-//            		 
-//            		 // 회원가입 성공 시 index.html 실패 시 /user/login_failed.html 로 이동
-//            		 if(DataBase.findUserById(paramCheck.get("userId")) != null ) {
-//            			 url = "/index.html";
-//            		 } else {
-//            			 url = "/user/login_failed.html";
-//            		 }
-//            	 } 
             	
-            	 System.out.println("마지막 url 확인 : " + url );
-            	 System.out.println("Test 성공하기를 ... 123 ");
+            	// get / post 방식으로 회원가입.. .
+            	 if(url.contains("?")) {	
+            		 // 쿼리 스트링이 있을 시 잘라버림 	
+            		 int index = url.indexOf("?");
+            		 
+            		 String requestPath = url.substring(0, index);
+            		 String params = url.substring(index+1);
+            		 
+            		 Map <String, String> paramCheck = HttpRequestUtils.parseQueryString(params);
+            		 
+            		 System.out.println("값 parsing Check 하기" + paramCheck);
+            		 
+            		 for(String keys : paramCheck.keySet()) {
+            			 System.out.println("값 Key Check : " + keys + " 값 Value Check " + paramCheck.get(keys) );
+            		 }
+            		 
+            		 User user = new User(paramCheck.get("userId") , paramCheck.get("password") , paramCheck.get("name"), paramCheck.get("email"));
+            		 
+            		 System.out.println("User Class 에 잘 담겼는지 Check : " + user);
+            		 	
+            		 DataBase.addUser(user);
+            		 
+            		 System.out.println("DB 들어간 값 확인 " + DataBase.findUserById(paramCheck.get("userId")) );
+            		 
+            		 // 회원가입 성공 시 index.html 실패 시 /user/login_failed.html 로 이동
+            		 if(DataBase.findUserById(paramCheck.get("userId")) != null ) {
+            			 url = "/index.html";
+            		 } else {
+            			 url = "/user/login_failed.html";
+            		 }
+            	 } else if( url.equals("/user/create")) {
+            		 System.out.println("Hello Create Post !");
+            	 }
+            	
             	 // 성공시 index.html 로 가지만 회원가입 후 로그인 누를 시 오류 발생 이것 해결하기 
             	 body = Files.readAllBytes(new File("./webapp" + url).toPath());
             	 System.out.println("마지막 Body 확인 : " + body);
             	 response200Header(dos, body.length);
                  responseBody(dos, body);	
+                 // get 방식으로 회원가입.. .
             }
             
         } catch (IOException e) {
